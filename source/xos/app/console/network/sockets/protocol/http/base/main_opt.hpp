db692a7c2701fd0c5995799fe6d7dbfddf9ea9ea
umachinae <umachinae@gmail.com>
2022-03-04 16:47:55 -0800

    updated source.

---
+++ source/xos/app/console/network/sockets/protocol/http/base/main_opt.hpp
@@ -0,0 +1,103 @@
+///////////////////////////////////////////////////////////////////////
+/// Copyright (c) 1988-2022 $organization$
+///
+/// This software is provided by the author and contributors ``as is'' 
+/// and any express or implied warranties, including, but not limited to, 
+/// the implied warranties of merchantability and fitness for a particular 
+/// purpose are disclaimed. In no event shall the author or contributors 
+/// be liable for any direct, indirect, incidental, special, exemplary, 
+/// or consequential damages (including, but not limited to, procurement 
+/// of substitute goods or services; loss of use, data, or profits; or 
+/// business interruption) however caused and on any theory of liability, 
+/// whether in contract, strict liability, or tort (including negligence 
+/// or otherwise) arising in any way out of the use of this software, 
+/// even if advised of the possibility of such damage.
+///
+///   File: main_opt.hpp
+///
+/// Author: $author$
+///   Date: 2/26/2022
+///////////////////////////////////////////////////////////////////////
+#ifndef XOS_APP_CONSOLE_NETWORK_SOCKETS_PROTOCOL_HTTP_BASE_MAIN_OPT_HPP
+#define XOS_APP_CONSOLE_NETWORK_SOCKETS_PROTOCOL_HTTP_BASE_MAIN_OPT_HPP
+
+#include "xos/app/console/protocol/http/base/main.hpp"
+#include "xos/app/console/network/sockets/protocol/xttp/base/main.hpp"
+
+namespace xos {
+namespace app {
+namespace console {
+namespace network {
+namespace sockets {
+namespace protocol {
+namespace http {
+namespace base {
+
+/// class main_optt
+template 
+<class TExtends = xos::app::console::network::sockets::protocol::xttp::base::maint
+ <xos::app::console::network::sockets::protocol::xttp::base::main_optt
+ <xos::app::console::protocol::http::base::maint
+ <xos::app::console::protocol::http::base::main_optt
+ <xos::app::console::protocol::xttp::base::maint
+ <xos::app::console::protocol::xttp::base::main_optt
+ <xos::app::console::network::sockets::protocol::xttp::base::maint
+ <xos::app::console::network::sockets::protocol::xttp::base::main_optt
+ <xos::app::console::network::sockets::base::maint<> > > > > > > > >, 
+ class TImplements = typename TExtends::implements>
+
+class exported main_optt: virtual public TImplements, public TExtends {
+public:
+    typedef TImplements implements;
+    typedef TExtends extends;
+    typedef main_optt derives;
+
+    typedef typename extends::char_t char_t;
+    typedef typename extends::end_char_t end_char_t;
+    enum { end_char = extends::end_char };
+    typedef typename extends::string_t string_t;
+    typedef typename extends::reader_t reader_t;
+    typedef typename extends::writer_t writer_t;
+    typedef typename extends::file_t file_t;
+
+    /// constructor / destructor
+    main_optt(): run_(0) {
+    }
+    virtual ~main_optt() {
+    }
+private:
+    main_optt(const main_optt& copy) {
+        throw exception(exception_unexpected);
+    }
+
+protected:
+    typedef typename extends::in_reader_t in_reader_t;
+    typedef typename extends::out_writer_t out_writer_t;
+    typedef typename extends::err_writer_t err_writer_t;
+
+    /// ...run
+    int (derives::*run_)(int argc, char_t** argv, char_t** env);
+    virtual int run(int argc, char_t** argv, char_t** env) {
+        int err = 0;
+        if ((run_)) {
+            err = (this->*run_)(argc, argv, env);
+        } else {
+            err = extends::run(argc, argv, env);
+        }
+        return err;
+    }
+
+protected:
+}; /// class main_optt
+typedef main_optt<> main_opt;
+
+} /// namespace base
+} /// namespace http
+} /// namespace protocol
+} /// namespace sockets
+} /// namespace network
+} /// namespace console
+} /// namespace app
+} /// namespace xos
+
+#endif /// ndef XOS_APP_CONSOLE_NETWORK_SOCKETS_PROTOCOL_HTTP_BASE_MAIN_OPT_HPP
